5460a584ae4211b4fbe5dcd371234cbbba530a99
Jeremy Mates <jmates@cpan.org>
2013-01-11 15:51:28 +0000

    new, plus routines to get and set intervals to use in modal calcs.

--- lib/Music/Canon.pm
+++ lib/Music/Canon.pm
@@ -1,11 +1,133 @@
+# -*- Perl -*-
+
 package Music::Canon;
 
 use 5.010000;
 use strict;
 use warnings;
 
+use Carp qw(croak);
+
+use List::Util qw/sum/;
+use Music::Scales qw/get_scale_nums is_scale/;
+use Scalar::Util qw/looks_like_number/;
+
 our $VERSION = '0.01';
 
+my $DEG_IN_SCALE = 12;
+
+########################################################################
+#
+# SUBROUTINES
+
+sub new {
+  my ( $class, %param ) = @_;
+  my $self = {};
+
+  # maybe
+  # $self->{_keep_state}        = $param{keep_state}        // 1;
+  # $self->{_sticky_state}      = $param{sticky_state}      // 0;
+
+  # maybe
+  # if ( exists $param{TODO_hook} ) {
+  #   croak "TODO_hook must be code ref"
+  #     unless ref $param{TODO_hook} eq 'CODE';
+  #   $self->{_TODO_hook} = $param{TODO_hook};
+  # }
+
+  $self->{_non_octave_scales} = $param{non_octave_scales} // 0;
+
+  bless $self, $class;
+
+  eval {
+    if ( exists $param{input_scale} ) {
+      $self->set_scale_intervals( 'input', $param{input_scale} );
+    }
+    if ( exists $param{output_scale} ) {
+      $self->set_scale_intervals( 'output', $param{output_scale} );
+    }
+  };
+  croak $@ if $@;
+
+  return $self;
+}
+
+sub modal_transmogrify {
+  my ( $self, $pset ) = @_;
+
+}
+
+sub get_scale_intervals {
+  my ( $self, $layer ) = @_;
+  unless ( $layer eq 'input' or $layer eq 'output' ) {
+    croak "unsupported layer (must be input or output)\n";
+  }
+  if ( !exists $self->{$layer}->{_asc_ints}
+    or !exists $self->{$layer}->{_dsc_ints} ) {
+    croak "scale intervals for $layer not previously set\n";
+  }
+  return $self->{$layer}->{_asc_ints}, $self->{$layer}->{_dsc_ints};
+}
+
+sub set_scale_intervals {
+  my ( $self, $layer, $scale, $dsc ) = @_;
+
+  unless ( $layer eq 'input' or $layer eq 'output' ) {
+    croak "unsupported layer (must be input or output)\n";
+  }
+
+  my ( @asc_nums, @dsc_nums );
+  if ( ref $scale eq 'ARRAY' ) {
+    # raw intervals passed as array ref
+    @asc_nums = @$scale;
+    croak "asc scales need intervals\n" if @asc_nums < 2;
+    if ( defined $dsc ) {
+      croak "descending intervals must be array reference\n"
+        unless ref $dsc eq 'ARRAY';
+      @dsc_nums = @$dsc;
+      croak "dsc scales need intervals\n" if @dsc_nums < 2;
+    } else {
+      # assume dsc is asc unless otherwise stated
+      @dsc_nums = @asc_nums;
+    }
+
+  } else {
+    # derive intervals via scale name via third-party module
+    croak "scale unknown to Music::Scales\n" unless is_scale($scale);
+    @asc_nums = get_scale_nums($scale);
+    @dsc_nums = get_scale_nums( $scale, 1 );
+  }
+
+  for my $n ( @asc_nums, @dsc_nums ) {
+    croak "intervals must be integers\n"
+      unless looks_like_number $n and $n =~ m/^[+-]?\d+$/;
+  }
+
+  for my $i ( 1 .. $#asc_nums ) {
+    push @{ $self->{$layer}->{_asc_ints} },
+      $asc_nums[$i] - $asc_nums[ $i - 1 ];
+  }
+  for my $i ( reverse 1 .. $#dsc_nums ) {
+    push @{ $self->{$layer}->{_dsc_ints} },
+      $dsc_nums[ $i - 1 ] - $dsc_nums[$i];
+  }
+
+  unless ( $self->{non_octave_scales} ) {
+    for
+      my $ref ( $self->{$layer}->{_asc_ints}, $self->{$layer}->{_dsc_ints} ) {
+      my $interval_sum = sum @$ref;
+      if ( $interval_sum < $DEG_IN_SCALE ) {
+        push @$ref, $DEG_IN_SCALE - $interval_sum;
+      } elsif ( $interval_sum > $DEG_IN_SCALE ) {
+        croak "scales >${DEG_IN_SCALE} require non_octave_scales param\n";
+      }
+    }
+  }
+  $self->{$layer}->{_asc_sum} = sum @{ $self->{$layer}->{_asc_ints} };
+  $self->{$layer}->{_dsc_sum} = sum @{ $self->{$layer}->{_dsc_ints} };
+
+  return $self;
+}
 
 1;
 __END__
@@ -21,7 +143,7 @@ Music::Canon - utility routines for musical canon construction
 
 =head1 DESCRIPTION
 
-TODO
+Utility routines for musical canon construction.
 
 =head1 SEE ALSO
 
--- t/Music-Canon.t
+++ t/Music-Canon.t
@@ -3,5 +3,21 @@
 use strict;
 use warnings;
 
-use Test::More tests => 1;
+use Test::More tests => 4;
 BEGIN { use_ok('Music::Canon') }
+
+my $mc = Music::Canon->new;
+isa_ok( $mc, 'Music::Canon' );
+
+my $resp = $mc->set_scale_intervals('input', 'major');
+isa_ok( $resp, 'Music::Canon' );
+
+# XXX not sure about order of things, how reverse is layed out, must
+# compare against 'major' vs. passing qw(2 2 1 2 2 2 1) vs. passing qw(2
+# 2 1 2 2 2) in vs. differing asc vs dsc intervals sets vs. what
+# whatever the modal transmogrifier sub needs to do its thing.
+is_deeply(
+  [ $mc->get_scale_intervals('input') ],
+  [ [qw(2 2 1 2 2 2 1)], [qw(2 2 1 2 2 2 1)] ],
+  'major intervals check'
+);
