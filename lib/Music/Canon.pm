28556d2a96fbe00518361b652163160174e6ec7d
Jeremy Mates <jmates@cpan.org>
2013-01-12 16:46:02 +0000

    Utility parameters and getters/setters plus exact_map for exact interval canon calculation.

--- lib/Music/Canon.pm
+++ lib/Music/Canon.pm
@@ -7,60 +7,91 @@ use strict;
 use warnings;
 
 use Carp qw(croak);
-
 use List::Util qw/sum/;
+
+use Music::LilyPondUtil ();    # transpose convenience
 use Music::Scales qw/get_scale_nums is_scale/;
-use Scalar::Util qw/looks_like_number/;
+use Scalar::Util qw/blessed looks_like_number/;
 
-our $VERSION = '0.01';
+our $VERSION = '0.10';
 
+# NOTE a new() param, below, but I have not thought about what changing
+# it would actually do. Use the $self entry in all subsequent code.
 my $DEG_IN_SCALE = 12;
 
 ########################################################################
 #
 # SUBROUTINES
 
-sub new {
-  my ( $class, %param ) = @_;
-  my $self = {};
-
-  # maybe
-  # $self->{_keep_state}        = $param{keep_state}        // 1;
-  # $self->{_sticky_state}      = $param{sticky_state}      // 0;
-
-  # maybe
-  # if ( exists $param{TODO_hook} ) {
-  #   croak "TODO_hook must be code ref"
-  #     unless ref $param{TODO_hook} eq 'CODE';
-  #   $self->{_TODO_hook} = $param{TODO_hook};
-  # }
-
-  $self->{_non_octave_scales} = $param{non_octave_scales} // 0;
-
-  bless $self, $class;
-
-  eval {
-    if ( exists $param{input_scale} ) {
-      $self->set_scale_intervals( 'input', $param{input_scale} );
-    }
-    if ( exists $param{output_scale} ) {
-      $self->set_scale_intervals( 'output', $param{output_scale} );
-    }
-  };
-  croak $@ if $@;
-
+sub contrary {
+  my ( $self, $contrary ) = @_;
+  $self->{_contrary} = $contrary ? 1 : 0;
   return $self;
 }
 
-sub modal_transmogrify {
-  my ( $self, $pset ) = @_;
+{
+  my ( $prev_input, $prev_output );
 
+  sub exact_map {
+    my ( $self, $phrase ) = @_;
+    $phrase = [ @_[ 1 .. $#_ ] ] if ref $phrase ne 'ARRAY';
+
+    my @new_phrase;
+    for my $e (@$phrase) {
+      my $pitch;
+      if ( !defined $e ) {
+        # presumably rests/silent bits
+        push @new_phrase, undef;
+        next;
+      } elsif ( blessed $e and $e->can('pitch') ) {
+        # XXX would be nice to be able to preserve the object if can set
+        # the new pitch, though that might need ->clone on whatever...
+        $pitch = $e->pitch;
+      } elsif ( looks_like_number $e) {
+        $pitch = $e;
+      } else {
+        # pass through unknowns
+        push @new_phrase, $e;
+        next;
+      }
+
+      my $new_pitch;
+      if ( !defined $prev_output ) {
+        $new_pitch = $pitch + $self->{_transpose};
+      } else {
+        my $delta = $pitch - $prev_input;
+        $delta *= -1 if $self->{_contrary};
+        $new_pitch = $prev_output + $delta;
+      }
+      push @new_phrase, $new_pitch;
+      $prev_input  = $pitch;
+      $prev_output = $new_pitch;
+    }
+
+    @new_phrase = reverse @new_phrase if $self->{_retrograde};
+
+    unless ( $self->{_keep_state} ) {
+      undef $prev_input;
+      undef $prev_output;
+    }
+
+    return @new_phrase;
+  }
+
+  sub exact_map_reset {
+    my ($self) = @_;
+    undef $prev_input;
+    undef $prev_output;
+  }
 }
+
+sub get_contrary   { $_[0]->{_contrary} }
+sub get_retrograde { $_[0]->{_retrograde} }
 
 sub get_scale_intervals {
   my ( $self, $layer ) = @_;
-  unless ( $layer eq 'input' or $layer eq 'output' ) {
-    croak "unsupported layer (must be input or output)\n";
+  unless ( defined $layer and ( $layer eq 'input' or $layer eq 'output' ) ) {
+    croak "unsupported layer (must be 'input' or 'output')\n";
   }
   if ( !exists $self->{$layer}->{_asc_ints}
     or !exists $self->{$layer}->{_dsc_ints} ) {
@@ -69,63 +100,233 @@ sub get_scale_intervals {
   return $self->{$layer}->{_asc_ints}, $self->{$layer}->{_dsc_ints};
 }
 
-sub set_scale_intervals {
-  my ( $self, $layer, $scale, $dsc ) = @_;
+sub get_transpose {
+  my ($self) = @_;
+  if ( !exists $self->{_transpose}
+    or !defined $self->{_transpose} ) {
+    $self->{_transpose} = 0;
+  }
+  return $self->{_transpose};
+}
 
-  unless ( $layer eq 'input' or $layer eq 'output' ) {
-    croak "unsupported layer (must be input or output)\n";
+{
+  my $prev_pitch;
+
+  sub modal_map {
+    my ( $self, $phrase ) = @_;
+    $phrase = [ @_[ 1 .. $#_ ] ] if ref $phrase ne 'ARRAY';
+
+    # default to major/major conversion
+    unless ( exists $self->{input} ) {
+      $self->scale_intervals( 'input', 'major' );
+    }
+    unless ( exists $self->{output} ) {
+      $self->scale_intervals( 'output', 'major' );
+    }
+
+    my @new_phrase;
+    for my $e (@$phrase) {
+      my $pitch;
+      if ( !defined $e ) {
+        # presumably rests/silent bits
+        push @new_phrase, undef;
+        next;
+      } elsif ( blessed $e and $e->can('pitch') ) {
+        # XXX would be nice to be able to preserve the object if can set
+        # the new pitch, though that might need ->clone on whatever...
+        $pitch = $e->pitch;
+      } elsif ( looks_like_number $e) {
+        $pitch = $e;
+      } else {
+        # pass through unknowns
+        push @new_phrase, $e;
+        next;
+      }
+
+      $self->{input}->{start_pitch} = $pitch
+        unless defined $self->{input}->{start_pitch};
+
+      my $new_pitch;
+      if ( !defined $prev_pitch ) {
+        # copy at transpose offset if nothing prior, set things up for
+        # subsequent modal foo
+        $new_pitch = $pitch + $self->{_transpose};
+        $self->{output}->{start_pitch} = $pitch
+          unless defined $self->{output}->{start_pitch};
+      } else {
+        # TODO
+      }
+
+      push @new_phrase, $new_pitch;
+      $prev_pitch = $pitch;
+    }
+
+    @new_phrase = reverse @new_phrase if $self->{_retrograde};
+
+    # XXX fiddle with state vars as necessary
+
+    return @new_phrase;
   }
 
-  my ( @asc_nums, @dsc_nums );
+  # XXX routines to get/set prev_pitch
+}
+
+# another way to influence modal_map (will be set automatically from the
+# phrase and transpose if unset, XXX need to think about persisting them
+# across calls, e.g. via sticky_state)
+sub modal_pitch {
+  my ( $self, $layer, $pitch ) = @_;
+
+  unless ( defined $layer and ( $layer eq 'input' or $layer eq 'output' ) ) {
+    croak "unsupported layer (must be 'input' or 'output')\n";
+  }
+  eval {
+    $self->{$layer}->{start_pitch} = $self->{_lyu}->notes2pitches($pitch);
+  };
+  croak $@ if $@;
+
+  return $self;
+}
+
+sub new {
+  my ( $class, %param ) = @_;
+  my $self = {};
+
+  $self->{_contrary} = exists $param{contrary} ? $param{contrary} ? 1 : 0 : 1;
+
+  $self->{_DEG_IN_SCALE} = int( $param{DEG_IN_SCALE} // $DEG_IN_SCALE );
+  if ( $self->{_DEG_IN_SCALE} < 2 ) {
+    croak("degrees in scale must be greater than one");
+  }
+
+  $self->{_lyu} =
+    exists $param{lyu}
+    ? $param{lyu}
+    : Music::LilyPondUtil->new( mode => 'absolute' );
+
+  $self->{_keep_state} =
+    exists $param{keep_state} ? $param{keep_state} ? 1 : 0 : 1;
+
+  $self->{_retrograde} =
+    exists $param{retrograde} ? $param{retrograde} ? 1 : 0 : 1;
+
+  if ( exists $param{transpose} ) {
+    eval {
+      $self->{_transpose} = $self->{_lyu}->notes2pitches( $param{transpose} );
+    };
+    croak $@ if $@;
+  } else {
+    $self->{_transpose} = 0;
+  }
+
+  # for exceptions in modal mapping (undefined note conversions)
+  if ( exists $param{modal_hook} ) {
+    croak "modal_hook must be code ref"
+      unless ref $param{modal_hook} eq 'CODE';
+    $self->{_modal_hook} = $param{modal_hook};
+  }
+
+  $self->{_non_octave_scales} = $param{non_octave_scales} // 0;
+
+  bless $self, $class;
+
+  eval {
+    if ( exists $param{input} ) {
+      $self->set_scale_intervals( 'input', $param{input} );
+    }
+    if ( exists $param{output} ) {
+      $self->set_scale_intervals( 'output', $param{output} );
+    }
+  };
+  croak $@ if $@;
+
+  return $self;
+}
+
+sub retrograde {
+  my ( $self, $retrograde ) = @_;
+  $self->{_retrograde} = $retrograde ? 1 : 0;
+  return $self;
+}
+
+sub scale_intervals {
+  my ( $self, $layer, $scale ) = @_;
+
+  unless ( defined $layer and ( $layer eq 'input' or $layer eq 'output' ) ) {
+    croak "unsupported layer (must be 'input' or 'output')\n";
+  }
+
   if ( ref $scale eq 'ARRAY' ) {
     # raw intervals passed as array ref
-    @asc_nums = @$scale;
-    croak "asc scales need intervals\n" if @asc_nums < 2;
-    if ( defined $dsc ) {
+    croak "intervals must be array of array refs"
+      unless ref $scale->[0] eq 'ARRAY';
+    croak "asc scales need intervals\n" if @{ $scale->[0] } < 2;
+    for my $n ( @{ $scale->[0] } ) {
+      croak "asc intervals must be integers\n"
+        unless looks_like_number $n and $n =~ m/^[+-]?\d+$/;
+    }
+    $self->{$layer}->{_asc_ints} = $scale->[0];
+
+    if ( @$scale > 1 ) {
       croak "descending intervals must be array reference\n"
-        unless ref $dsc eq 'ARRAY';
-      @dsc_nums = @$dsc;
-      croak "dsc scales need intervals\n" if @dsc_nums < 2;
+        unless ref $scale->[1] eq 'ARRAY';
+      croak "dsc scales need intervals\n" if @{ $scale->[1] } < 2;
+      for my $n ( @{ $scale->[1] } ) {
+        croak "asc intervals must be integers\n"
+          unless looks_like_number $n and $n =~ m/^[+-]?\d+$/;
+      }
+
+      $self->{$layer}->{_dsc_ints} = $scale->[1];
+
     } else {
-      # assume dsc is asc unless otherwise stated
-      @dsc_nums = @asc_nums;
+      # assume dsc is asc (true, excepting melodic minor and whatnot)
+      $self->{$layer}->{_dsc_ints} = $self->{$layer}->{_asc_ints};
     }
 
   } else {
     # derive intervals via scale name via third-party module
     croak "scale unknown to Music::Scales\n" unless is_scale($scale);
-    @asc_nums = get_scale_nums($scale);
-    @dsc_nums = get_scale_nums( $scale, 1 );
-  }
+    my @asc_nums = get_scale_nums($scale);
+    my @dsc_nums = get_scale_nums( $scale, 1 );
 
-  for my $n ( @asc_nums, @dsc_nums ) {
-    croak "intervals must be integers\n"
-      unless looks_like_number $n and $n =~ m/^[+-]?\d+$/;
-  }
+    # do not expect this to be a problem, but if it is...
+    for my $n ( @asc_nums, @dsc_nums ) {
+      die "intervals from Music::Scales not integers"
+        unless looks_like_number $n and $n =~ m/^[+-]?\d+$/;
+    }
 
-  for my $i ( 1 .. $#asc_nums ) {
-    push @{ $self->{$layer}->{_asc_ints} },
-      $asc_nums[$i] - $asc_nums[ $i - 1 ];
-  }
-  for my $i ( reverse 1 .. $#dsc_nums ) {
-    push @{ $self->{$layer}->{_dsc_ints} },
-      $dsc_nums[ $i - 1 ] - $dsc_nums[$i];
+    for my $i ( 1 .. $#asc_nums ) {
+      push @{ $self->{$layer}->{_asc_ints} },
+        $asc_nums[$i] - $asc_nums[ $i - 1 ];
+    }
+    for my $i ( reverse 1 .. $#dsc_nums ) {
+      push @{ $self->{$layer}->{_dsc_ints} },
+        $dsc_nums[ $i - 1 ] - $dsc_nums[$i];
+    }
   }
 
   unless ( $self->{non_octave_scales} ) {
     for
       my $ref ( $self->{$layer}->{_asc_ints}, $self->{$layer}->{_dsc_ints} ) {
       my $interval_sum = sum @$ref;
-      if ( $interval_sum < $DEG_IN_SCALE ) {
-        push @$ref, $DEG_IN_SCALE - $interval_sum;
-      } elsif ( $interval_sum > $DEG_IN_SCALE ) {
-        croak "scales >${DEG_IN_SCALE} require non_octave_scales param\n";
+      if ( $interval_sum < $self->{_DEG_IN_SCALE} ) {
+        push @$ref, $self->{_DEG_IN_SCALE} - $interval_sum;
+      } elsif ( $interval_sum > $self->{_DEG_IN_SCALE} ) {
+        croak "non-octave scales require non_octave_scales param\n";
       }
     }
   }
   $self->{$layer}->{_asc_sum} = sum @{ $self->{$layer}->{_asc_ints} };
   $self->{$layer}->{_dsc_sum} = sum @{ $self->{$layer}->{_dsc_ints} };
 
+  return $self;
+}
+
+sub transpose {
+  my ( $self, $transpose ) = @_;
+  $transpose //= 0;
+  eval { $self->{_transpose} = $self->{_lyu}->notes2pitches($transpose); };
+  croak $@ if $@;
   return $self;
 }
 
--- t/Music-Canon.t
+++ t/Music-Canon.t
@@ -3,21 +3,72 @@
 use strict;
 use warnings;
 
-use Test::More tests => 4;
+use Test::More tests => 14;
 BEGIN { use_ok('Music::Canon') }
+
+eval 'use Test::Differences';    # display convenience
+my $deeply = $@ ? \&is_deeply : \&eq_or_diff;
+
+########################################################################
+#
+# Defaults and Initial Mode Setup
 
 my $mc = Music::Canon->new;
 isa_ok( $mc, 'Music::Canon' );
 
-my $resp = $mc->set_scale_intervals('input', 'major');
+# defaults
+is( $mc->get_transpose, 0, 'default transpose' );
+is( $mc->get_contrary,  1, 'default contrary' );
+is( $mc->get_retrograde,  1, 'default retrograde' );
+
+# set intervals by scale name (via Music::Scales)
+my $resp = $mc->scale_intervals( 'input', 'major' );
 isa_ok( $resp, 'Music::Canon' );
+
+# set intervals manually (also major) - XXX think about whether reverse
+# should be top down, as descending is how musician would think about it
+$mc->scale_intervals( 'output',
+  [ [qw/2 2 1 2 2 2 1/], [qw/2 2 1 2 2 2 1/] ] );
 
 # XXX not sure about order of things, how reverse is layed out, must
 # compare against 'major' vs. passing qw(2 2 1 2 2 2 1) vs. passing qw(2
 # 2 1 2 2 2) in vs. differing asc vs dsc intervals sets vs. what
 # whatever the modal transmogrifier sub needs to do its thing.
-is_deeply(
+$deeply->(
   [ $mc->get_scale_intervals('input') ],
   [ [qw(2 2 1 2 2 2 1)], [qw(2 2 1 2 2 2 1)] ],
-  'major intervals check'
+  'major intervals check input'
 );
+$deeply->(
+  [ $mc->get_scale_intervals('output') ],
+  [ [qw(2 2 1 2 2 2 1)], [qw(2 2 1 2 2 2 1)] ],
+  'major intervals check output'
+);
+
+########################################################################
+#
+# Mappings
+
+$deeply->( [ $mc->exact_map(qw/0 1 2/) ], [qw/-2 -1 0/], 'exact map' );
+
+########################################################################
+#
+# getters/setters
+
+$mc = Music::Canon->new;
+
+$mc->contrary(0);
+is( $mc->get_contrary, 0, 'set contrary false' );
+$mc->contrary(1);
+is( $mc->get_contrary, 1, 'set contrary true' );
+
+$mc->transpose(q{c'});
+is( $mc->get_transpose, 60, 'transpose to lilypond note' );
+
+# some value that should not be set by default
+my $rand_transpose = 200 + int rand 100;
+$mc->transpose($rand_transpose);
+is( $mc->get_transpose, $rand_transpose, 'get rand transpose' );
+
+$mc->transpose;
+is( $mc->get_transpose, 0, 'reset transpose' );
