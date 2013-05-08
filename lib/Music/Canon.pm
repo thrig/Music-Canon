22e2c5f96dbe7d7884ac47db62d661c3aa90d4f8
Jeremy Mates <jmates@cpan.org>
2013-05-08 04:34:49 +0000

    Confirm no changes needed for Music::AtonalUtil refactoring.

--- lib/Music/Canon.pm
+++ lib/Music/Canon.pm
@@ -18,7 +18,7 @@ use Music::LilyPondUtil ();    # transpose convenience
 use Music::Scales qw/get_scale_nums is_scale/;
 use Scalar::Util qw/blessed looks_like_number/;
 
-our $VERSION = '0.51';
+our $VERSION = '0.52';
 
 # NOTE a new() param, below, but I have not thought about what changing
 # it would actually do. Use the $self entry in all subsequent code.
